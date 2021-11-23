3ae8e5417f5adad9df8e448755432ea8e280db37
AlfonsoAD <alfonsad097@gmail.com>
2021-11-23 00:31:30 -0600

    Actualización

--- Parcial 3/phpPDO/Conectar.php
+++ Parcial 3/phpPDO/Conectar.php
@@ -3,11 +3,11 @@
 $hostname='localhost';
 $database='a18100149';
 $username='root';
-$password='';
+$password='jesus123';
 
 try {
       $con = new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
-      echo "hola, si me concté";
+      echo "hola, si me concté <br>";
 } catch(PDOException $e) {
       echo "Error de conexion a la base de datos";
       echo $e->getMessage();
