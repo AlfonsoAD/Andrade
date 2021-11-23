9854b537a7e26506e4f092d52466745a492569a7
AlfonsoAD <alfonsad097@gmail.com>
2021-11-22 21:50:08 -0600

    Update

--- Parcial 3/phpPDO/Conectar.php
+++ Parcial 3/phpPDO/Conectar.php
@@ -7,6 +7,7 @@ $password='';
 
 try {
       $con = new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
+      echo "hola, si me concté";
 } catch(PDOException $e) {
       echo "Error de conexion a la base de datos";
       echo $e->getMessage();
--- Parcial 3/phpPDO/Consulta.php
+++ Parcial 3/phpPDO/Consulta.php
@@ -7,16 +7,16 @@ try {
        $query->execute();
 
         while ($row = $query->fetch()) {
-            echo $row['idJugador'].'-'.
-                 $row['Nombre'].'-'.
-                 $row['ApellidoP'].'-'.
-                 $row['ApellidoM'].'-'.
+            echo $row['IdJugador'].', '.
+                 $row['Nombre'].', '.
+                 $row['ApellidoP'].', '.
+                 $row['ApellidoM'].', '.
                  $row['FechaNac'].'<br>';
         }
         $query->closeCursor();
 
 } catch(PDOException $e) {
-        echo "Error de consulta a la base de datos:(";
+        echo "Error de consulta a la base de datos";
         echo $e->getMessage();
 }
 ?>
